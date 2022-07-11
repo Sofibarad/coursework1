e69c955e186520dbff9e9746643d8c90e7cfb797
Sofibarad <sofibarad@gmail.com>
2022-07-11 22:03:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+In this coursework we will deal with simple Expense and Income tables for private company usage with Pandas and Python
---
+++ newtesting.py
@@ -0,0 +1,14 @@
+import os
+actual_cost = float(input(" Please Enter the Actual Transportation Price: "))
+sale_amount = float(input(" Please Enter the Sales Transportation Amount: "))
+ 
+if(actual_cost > sale_amount):
+    amount = actual_cost - sale_amount
+    print("Total Loss Amount = {0}".format(amount))
+elif(sale_amount > actual_cost):
+    amount = sale_amount - actual_cost
+    print("Total Profit = {0}".format(amount))
+else:
+    print("No Profit No Loss!!!")
+
+
